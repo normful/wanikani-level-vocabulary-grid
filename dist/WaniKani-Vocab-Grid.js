433a92f5b2ac5469d0d203609bbab93ff645b3b2
Norman Sue <normful@gmail.com>
2016-07-24 02:58:03 -0700

    [dist] bump version

--- dist/WaniKani-Vocab-Grid.js
+++ dist/WaniKani-Vocab-Grid.js
@@ -3,7 +3,7 @@
 // @description  Rearranges the WaniKani Level Vocabulary into a grid
 // @author       Norman Sue
 // @namespace    normful
-// @version      0.3.0
+// @version      0.3.1
 // @license      MIT
 // @match        http://www.wanikani.com/level*
 // @match        http://www.wanikani.com/vocabulary*
