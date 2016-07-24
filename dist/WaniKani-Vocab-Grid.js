533700e5d56f635a7d6f79de3336ad98bc383f4d
Norman Sue <normful@gmail.com>
2016-07-24 02:57:25 -0700

    [js] use non jQuery-method of inserting CSS stylesheet

--- dist/WaniKani-Vocab-Grid.js
+++ dist/WaniKani-Vocab-Grid.js
@@ -18,5 +18,15 @@
     'use strict';
     var cssUrl = 'https://normful.github.io/WaniKani-Vocab-Grid-Plain.css';
     var cssId  = 'normful-wanikani-level-vocabulary-grid';
-    $("head").append("<link rel='stylesheet' id='" + cssId + " href='" + cssUrl + "' type='text/css'/>");
+
+    if (!document.getElementById(cssId)) {
+        var head  = document.getElementsByTagName('head')[0];
+        var link  = document.createElement('link');
+        link.id    = cssId;
+        link.rel   = 'stylesheet';
+        link.type  = 'text/css';
+        link.href  = cssUrl;
+        link.media = 'all';
+        head.appendChild(link);
+    }
 })();
